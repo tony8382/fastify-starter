460d0fc72da0430ba39980b43a1f84f35f2f4fde
Li-Yu, Yang <tony830802@gmail.com>
2023-02-27 11:04:13 +0800

    add test for path parameter

--- src/routes/hello/index.ts
+++ src/routes/hello/index.ts
@@ -1,8 +1,19 @@
 import { FastifyInstance, FastifyPluginAsync, FastifyReply, FastifyRequest, FastifyServerOptions } from "fastify";
 
+type HelloRequest = FastifyRequest<{
+  Params: { name: string }
+}>
+
 const example: FastifyPluginAsync = async (fastify: FastifyInstance, opts: FastifyServerOptions): Promise<void> => {
   fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+    request.log.debug(null, "GGGGGGG: %s ", "123")
     return 'this is an GGGFFGG'
+  })
+  fastify.get('/:name', async (request: HelloRequest, reply: FastifyReply) => {
+    const { name } = request.params;
+
+    request.log.info(null, "GGGGGGG: %s ", name)
+    return {test:name}
   })
 }
 
